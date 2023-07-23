7550192a2b70d4b0fc7b57ff041e1163e8453193
Abraham Orherhe <aeorherhe@icloud.com>
2023-07-23 13:19:30 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// select elements
+// const adviceID = document.querySelector(".code");
+const adviceCode = document.querySelector(".advice-code");
+const advices = document.querySelector(".advice");
+const diceBtn = document.querySelector(".dice-btn");
+const url = "https://api.adviceslip.com/advice";
+
+const fetchAdvice = async () => {
+  adviceCode.textContent = "Loading...";
+  advices.innerHTML = `<div class="loading-state"></div>`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    const {
+      slip: { id, advice },
+    } = data;
+    adviceCode.innerHTML = `advice #<span class="code">${id}</span>`;
+    advices.innerHTML = advice;
+  } catch (error) {
+    adviceCode.textContent = "Error";
+    adviceCode.style.color = "#ef0c26";
+    advices.textContent = "Sorry, there was an error. Please try again later.";
+  }
+};
+
+window.addEventListener("DOMContentLoaded", fetchAdvice);
+
+diceBtn.addEventListener("click", () => {
+  fetchAdvice();
+  diceBtn.classList.toggle("clicked");
+});
